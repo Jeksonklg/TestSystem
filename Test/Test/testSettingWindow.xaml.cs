c325224fb8eee1b7d1c4871380d4122c6e994783
Jekson <Jeksonklg@mail.ru>
2015-04-09 19:33:10 +0300

    Залил интерфейс проги.

---
+++ Test/Test/testSettingWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for testSettingWindow.xaml
+    /// </summary>
+    public partial class testSettingWindow : Window
+    {
+        public testSettingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void slide_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
